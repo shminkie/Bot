0c31c55c5237125f951746af571f2ab7d1346505
Jesse McKee <jessemckee97@gmail.com>
2022-01-12 23:01:22 -0600

    Update scripts and enchance slash commands

--- src/index.js
+++ src/index.js
@@ -16,11 +16,12 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply('Pong!');
 	}
 	else if (commandName === 'server') {
-		await interaction.reply('Server info.');
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	}
 	else if (commandName === 'user') {
-		await interaction.reply('User info.');
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	}
 });
+
 
 client.login(token);
