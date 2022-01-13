9b5c4e5f56a007d98dba4c8a8ba10dc8e4ac7785
Jesse McKee <jessemckee97@gmail.com>
2022-01-12 22:24:24 -0600

    Fix formatting

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const { Client, Intents } = require('discord.js');
-const {channelId, token } = require('./config.json');
+const {Client, Intents} = require('discord.js');
+const {channelId, token} = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
 client.once('ready', async () => {
     console.log('Ready!');
 });
 
 client.on("messageCreate", msg => {
-    if(msg.content.trim() === "!ping") {
+    if (msg.content.trim() === "!ping") {
         const channel = client.channels.cache.get(channelId)
         channel.send("pong!");
     }
