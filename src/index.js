3498c9b30f8ebbe508b8b8349a31ecd6491a25e6
Jesse McKee <jessemckee97@gmail.com>
2022-01-12 22:54:11 -0600

    Introduced linter to the code, and formatted accordingly.

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('../config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', async () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	}
+	else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+client.login(token);
