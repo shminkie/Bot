c186623bd39fb164174b1719e80dec9d33af0131
Jesse McKee <jessemckee97@gmail.com>
2022-01-12 22:33:06 -0600

    Introduced linter to the code, and formatted accordingly.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const {Client, Intents} = require('discord.js');
-const {channelId, token} = require('./config.json');
+const { Client, Intents } = require('discord.js');
+const { channelId, token } = require('./config.json');
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', async () => {
-    console.log('Ready!');
+	console.log('Ready!');
 });
 
-client.on("messageCreate", msg => {
-    if (msg.content.trim() === "!ping") {
-        const channel = client.channels.cache.get(channelId)
-        channel.send("pong!");
-    }
-})
+client.on('messageCreate', msg => {
+	if (msg.content.trim() === '!ping') {
+		const channel = client.channels.cache.get(channelId);
+		channel.send('pong!');
+	}
+});
 
 client.login(token);
