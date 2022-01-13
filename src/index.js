ae2381173e73174adb68dbf8cc30a92af63d74bf
Jesse McKee <jessemckee97@gmail.com>
2022-01-13 01:29:42 -0600

    Add commandhandeler for slashcommands added avatar command

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
-const { Client, Intents } = require('discord.js');
-const { token } = require('../config.json');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [
+	Intents.FLAGS.GUILD_MESSAGES,
+	Intents.FLAGS.GUILD_INVITES,
+	Intents.FLAGS.GUILDS,
+	Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+],
+});
 
-client.once('ready', async () => {
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
 	}
-	else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	}
-	else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
